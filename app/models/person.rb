835d20aad687b8268b3a210d094052fc9b39540d
caseywatts <casey.s.watts@gmail.com>
2014-09-16 01:03:53 -0400

    extract ldap code to a separate class

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,11 +1,6 @@
 require 'net-ldap'
 
 class Person < ActiveRecord::Base
-  LDAP_HOST = 'directory.yale.edu'
-  LDAP_PORT = 389
-  LDAP_BASE = 'ou=People,o=yale.edu'
-  LDAP_ATTRS = %w(uid givenname sn mail collegename college class UPI)
-
   establish_connection "oracle"
   self.table_name = "people.ppl_identity_v"
 
@@ -20,43 +15,9 @@ class Person < ActiveRecord::Base
 
   end
 
-  # test with Person.ldap_lookup_by_upi("12714662")
-  def ldap_lookup_by_upi(upi)
-    ldap = Net::LDAP.new host: LDAP_HOST, port: LDAP_PORT
-    upifilter = Net::LDAP::Filter.eq('UPI', upi)
-    ldap_response = ldap.search(base: LDAP_BASE,
-                     filter: upifilter,
-                     attributes: LDAP_ATTRS)
-    assign_ldap_attributes(ldap_response)
-  end
-
-  def assign_ldap_attributes(ldap_response)
-    @first_name = ldap_response[0][:givenname][0]
-    @last_name = ldap_response[0][:sn][0]
-    @upi = ldap_response[0][:UPI][0]
-
-    # not everyone has these
-    @netid = ldap_response[0][:uid][0] || ""
-    @email = ldap_response[0][:mail][0] || ""
-    @collegename = ldap_response[0][:collegename][0] || ""
-    @college = ldap_response[0][:college][0] || ""
-    @class_year = ldap_response[0][:class][0] || ""
-
-    return {
-      first_name: @first_name,
-      last_name: @last_name,
-      yale_upi: @upi,
-      netid: @netid
-      # email: @email,
-      # collegename: @collegename,
-      # college: @college,
-      # class_year: @class_year
-    }
-  end
-
   # test with Person.new(upi: "12714662")
   def initialize(attributes)
-    attributes = ldap_lookup_by_upi(attributes[:upi])
+    attributes = LDAP.lookup_by_upi(attributes[:upi])
     super
   end
 
