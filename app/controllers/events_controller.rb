1c257ae3ea54e92eb128f4d94fd40c2ba81804a2
caseywatts <casey.s.watts@gmail.com>
2014-09-21 23:53:48 -0400

    reverse sort order of events page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,7 +7,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.all.reverse
   end
 
   # GET /events/1
