20a6b91f5da075280727494b30a9efe3cde3e45f
caseywatts <casey.s.watts@gmail.com>
2014-09-22 12:37:11 -0400

    display unknown affiliation if no school or year information present

--- app/controllers/distribution_controller.rb
+++ app/controllers/distribution_controller.rb
@@ -16,6 +16,7 @@ class DistributionController < ApplicationController
       @name = attributes[:first_name] + " " + attributes[:last_name]
       @email = attributes[:email]
       @affiliation = attributes[:school] + " " + attributes[:college_abbreviation] + " " + attributes[:class_year]
+      @affiliation = "Unknown Affiliation" if @affiliation.blank?
       @sn_destination_url = "https://yale.service-now.com/incident.do?sys_id=-1&sysparm_query=caller_id=" + sys_id + "^u_contact=" + sys_id
       @phonebook_destination_url = "http://directory.yale.edu/phonebook/index.htm?searchString=upi%3D" + upi
     end
