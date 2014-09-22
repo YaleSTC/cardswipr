e6a56fcbf06aea3fa12b6ef2ffa6cbeb3882ab2f
caseywatts <casey.s.watts@gmail.com>
2014-09-21 21:55:40 -0400

    the rest of attendance_entries renaming

---
+++ app/controllers/attendance_entries_controller.rb
@@ -0,0 +1,79 @@
+class AttendanceEntriesController < ApplicationController
+  before_action :set_attendance_entry, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
+
+  # GET /attendance_entries
+  # GET /attendance_entries.json
+  def index
+    @event = Event.find(params[:event_id])
+    @attendance_entries = @event.attendance_entries
+  end
+
+  # GET /attendance_entries/1
+  # GET /attendance_entries/1.json
+  def show
+    @attendance_entry = AttendanceEntry.find(params[:id])
+  end
+
+  # GET /attendance_entries/new
+  def new
+    @event = Event.find(params[:event_id])
+    @attendance_entry = AttendanceEntry.new(:event => @event)
+  end
+
+  # GET /attendance_entries/1/edit
+  def edit
+  end
+
+  # POST /attendance_entries
+  # POST /attendance_entries.json
+  def create
+    # @event = Event.find(params[:event_id])
+    @attendance_entry = AttendanceEntry.new(attendance_entry_params)
+
+    respond_to do |format|
+      if @attendance_entry.save
+        format.html { redirect_to event_attendance_entries_path(@attendance_entry.event), notice: 'Event attendance entry was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @attendance_entry }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @attendance_entry.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /attendance_entries/1
+  # PATCH/PUT /attendance_entries/1.json
+  def update
+    respond_to do |format|
+      if @attendance_entry.update(attendance_entry_params)
+        format.html { redirect_to @attendance_entry, notice: 'Event attendance entry was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @attendance_entry.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /attendance_entries/1
+  # DELETE /attendance_entries/1.json
+  def destroy
+    @attendance_entry.destroy
+    respond_to do |format|
+      format.html { redirect_to attendance_entries_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_attendance_entry
+      @attendance_entry = AttendanceEntry.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def attendance_entry_params
+      params.require(:attendance_entry).permit(:first_name, :nickname, :last_name, :upi, :netid, :email, :college_name, :college_abbreviation, :class_year, :school, :telephone, :address, :event_id)
+    end
+end
---
+++ app/models/attendance_entry.rb
@@ -0,0 +1,38 @@
+class AttendanceEntry < ActiveRecord::Base
+
+#has_many people
+belongs_to :event
+
+#within an event, upi must be unique
+after_create :get_ldap_attributes
+
+  # EventAttendanceEntry should be initialized with a upi
+  # @param [string] upi = universal personal identification
+  # @example
+  #   EventAttendanceEntry.new(upi: "12714662", event: 1)
+  def get_ldap_attributes
+    attributes = YaleLDAP.lookup(upi: upi.to_s)
+      .slice(:first_name, :nickname, :last_name, :upi, :netid,
+        :email, :college_name, :college_abbreviation,
+        :class_year, :school, :telephone, :address)
+    self.update_attributes(attributes)
+  end
+
+  def name
+    if nickname.blank?
+      return first_name + " " + last_name
+    else
+      return nickname + " " + last_name
+    end
+  end
+
+  def recorded?
+    return EventAttendanceEntry.find_by(upi: self.upi).present?
+  end
+
+  def record
+    return false if recorded?
+    EventAttendanceEntry.create(self.upi)
+  end
+
+end
