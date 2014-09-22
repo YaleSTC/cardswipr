7675e35440633dcf5d72c8b19780aac7fb4f9fa5
caseywatts <casey.s.watts@gmail.com>
2014-09-21 23:03:52 -0400

    add email and affiliation to person lookup

--- app/controllers/distribution_controller.rb
+++ app/controllers/distribution_controller.rb
@@ -10,12 +10,12 @@ class DistributionController < ApplicationController
     authorize! :read, :personlookup
     if params[:query]
       attributes = YaleIDLookup.lookup(params[:query])
-
-      sys_id = ServiceNow::User.find(attributes.netid).sys_id
+      upi = attributes[:upi]
+      sys_id = ServiceNow::User.find(attributes[:netid]).sys_id
       
       @name = attributes[:first_name] + attributes[:last_name]
-      # @email = person.email
-      # @affiliation = "Undergraduate"
+      @email = attributes[:email]
+      @affiliation = attributes[:school] + " " + attributes[:college_abbreviation] + " " + attributes[:class_year]
       @sn_destination_url = "https://yale.service-now.com/incident.do?sys_id=-1&sysparm_query=caller_id=" + sys_id + "^u_contact=" + sys_id
       @phonebook_destination_url = "http://directory.yale.edu/phonebook/index.htm?searchString=upi%3D" + upi
     end
