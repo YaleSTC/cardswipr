085239d1b3921c2fdba92910e889d65b10d468de
caseywatts <casey.s.watts@gmail.com>
2014-09-26 19:30:58 -0400

    allow user_ids to be saved in the events form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -128,6 +128,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :description)
+      params.require(:event).permit(:title, :description, :user_ids => [])
     end
 end
