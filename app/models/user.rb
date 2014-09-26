460e7012166470871df5dae07ee36b866a1ffd67
caseywatts <casey.s.watts@gmail.com>
2014-09-26 18:42:11 -0400

    auto-load first_name and last_name from LDAP, some user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
+  validates :netid, presence: true, uniqueness: true
   has_and_belongs_to_many :event
+
+  after_create :get_ldap_attributes
+
+  def get_ldap_attributes
+    attributes = YaleLDAP.lookup(netid: netid)
+      .slice(:first_name, :last_name, :netid)
+    self.update_attributes(attributes)
+  end
+
 end
