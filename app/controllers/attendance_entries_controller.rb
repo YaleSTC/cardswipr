05fc99e28b87891c92fcc9469770484418989672
caseywatts <casey.s.watts@gmail.com>
2014-09-27 16:21:17 -0400

    let cancancan load_and_authorize_resource do its job

--- app/controllers/attendance_entries_controller.rb
+++ app/controllers/attendance_entries_controller.rb
@@ -7,8 +7,6 @@ class AttendanceEntriesController < ApplicationController
   # GET /attendance_entries
   # GET /attendance_entries.json
   def index
-    @event = Event.find(params[:event_id])
-    @attendance_entries = @event.attendance_entries
     @count = @attendance_entries.count
 
     respond_to do |format|
@@ -20,13 +18,11 @@ class AttendanceEntriesController < ApplicationController
   # GET /attendance_entries/1
   # GET /attendance_entries/1.json
   def show
-    @attendance_entry = AttendanceEntry.find(params[:id])
   end
 
   # GET /attendance_entries/new
   def new
-    @event = Event.find(params[:event_id])
-    @attendance_entry = AttendanceEntry.new(:event => @event)
+    @attendance_entry.event = @event
   end
 
   # GET /attendance_entries/1/edit
@@ -36,9 +32,6 @@ class AttendanceEntriesController < ApplicationController
   # POST /attendance_entries
   # POST /attendance_entries.json
   def create
-    # @event = Event.find(params[:event_id])
-    @attendance_entry = AttendanceEntry.new(attendance_entry_params)
-
     respond_to do |format|
       if @attendance_entry.save
         format.html { redirect_to event_attendance_entries_path(@attendance_entry.event), notice: 'Event attendance entry was successfully created.' }
@@ -67,7 +60,6 @@ class AttendanceEntriesController < ApplicationController
   # DELETE /attendance_entries/1
   # DELETE /attendance_entries/1.json
   def destroy
-    @event = @attendance_entry.event
     @attendance_entry.destroy
     respond_to do |format|
       format.html { redirect_to event_attendance_entries_path(@event) }
@@ -76,11 +68,6 @@ class AttendanceEntriesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    # def set_attendance_entry
-    #   @attendance_entry = AttendanceEntry.find(params[:id])
-    # end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def attendance_entry_params
       params.require(:attendance_entry).permit(:first_name, :nickname, :last_name, :upi, :netid, :email, :college_name, :college_abbreviation, :class_year, :school, :telephone, :address, :event_id)
