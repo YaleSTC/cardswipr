da60d5ded17da59ed91d8e68c61e3a1975c207bb
caseywatts <casey.s.watts@gmail.com>
2014-09-26 23:23:09 -0400

    remove unused through association

--- app/models/attendance_entry.rb
+++ app/models/attendance_entry.rb
@@ -1,8 +1,6 @@
 class AttendanceEntry < ActiveRecord::Base
 
-#has_many people
 belongs_to :event
-has_many :users, through: :event
 validates :event, presence: true
 validates :upi, :uniqueness => { :scope => :event, :message => "This person has already been checked into this event." }
 
