3ee04f9a0656246fe98b544756d3490e8c76da3a
caseywatts <casey.s.watts@gmail.com>
2014-09-28 20:12:06 -0400

    cleanup unnecessary app_setup method

--- spec/features/swipe_into_events_spec.rb
+++ spec/features/swipe_into_events_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe "SwipeIntoEvents", :type => :feature do
   before :each do
-    app_setup
     # @user = create(:user)
     @event = create(:event)
     sign_in(@event.users.first.netid)
@@ -18,11 +17,6 @@ describe "SwipeIntoEvents", :type => :feature do
     visit event_swipe_path(@event)
     fill_in 'query', with: "csw3"
     click_on('Submit')
-    save_and_open_page
     expect(page).to have_content 'success'
   end
-end
-
-def app_setup
-  
 end
