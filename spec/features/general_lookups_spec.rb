34ea4fb66c52352dcafed2d34c4f43fdfdf4868b
caseywatts <casey.s.watts@gmail.com>
2014-09-28 20:11:33 -0400

    add two tests for general lookup

--- spec/features/general_lookups_spec.rb
+++ spec/features/general_lookups_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe "GeneralLookups", :type => :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  before :each do
+    @user = create(:user)
+    sign_in(@user.netid)
+  end
+
+  it "can navigate to the general lookups page" do
+    visit distribution_personlookup_path
+    # brittle but only unique identifier so far
+    expect(page).to have_content 'Card Swipe or NetID or Yale Email'
+  end
+
+  it "can look someone up by netid" do
+    visit distribution_personlookup_path
+    fill_in 'query', with: "csw3"
+    click_on('Submit')
+    expect(page).to have_content 'Casey Watts'
+  end
 end
