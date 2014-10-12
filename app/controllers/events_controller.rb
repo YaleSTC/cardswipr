1fa60ce2aa55bd3f3ed737195940d3f387c74a06
caseywatts <casey.s.watts@gmail.com>
2014-10-12 19:40:35 -0400

    wipe_attendance needs @event manually done, I'd forgotten

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -139,7 +139,7 @@ class EventsController < ApplicationController
 
 
   def wipe_attendance
-    # @event = Event.find(params[:event_id])
+    @event = Event.find(params[:event_id])
     @event.attendance_entries.destroy_all
     flash[:notice] = "All attendance entries for this event have been wiped."
     redirect_to event_attendance_entries_path(@event)
