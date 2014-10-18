eaa83f5ef7b09ef9fd23343aa8a428c3e44484ad
caseywatts <casey.s.watts@gmail.com>
2014-10-17 22:01:00 -0400

    hard-coded attendance list, basic filtering

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -1,4 +1,37 @@
 angular.module('myApp')
     .controller('HomeCtrl', function ($scope) {
-        $scope.things = ['Angular', 'Rails 4.1', 'UI Router', 'Together!!'];
+      $scope.people = [
+            {
+                id: 0,
+                name: 'Addison',
+                present: false
+            },
+            {
+                id: 1,
+                name: 'Shane',
+                present: false
+            },
+            {
+                id: 2,
+                name: 'Sei',
+                present: false
+            }
+        ];
+        $scope.newPerson = null;
+        $scope.addNew = function(){
+            if ($scope.newPerson != null && $scope.newPerson != "") {
+                $scope.people.push({
+                    id: $scope.people.length,
+                    name: $scope.newPerson,
+                    live: true,
+                    music: []
+                });
+            }
+        };
+        $scope.markPresent = function(person){
+            person.present = true;
+        };
+        $scope.markAbsent = function(person){
+            person.present = false;
+        };
     });
