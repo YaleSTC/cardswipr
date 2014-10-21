28d36c965a31f0e274dd4b271048f41f9f2ec1dd
caseywatts <casey.s.watts@gmail.com>
2014-10-21 12:46:33 -0400

    add curriculum and organization to walk in center lookup

--- app/controllers/distribution_controller.rb
+++ app/controllers/distribution_controller.rb
@@ -16,7 +16,11 @@ class DistributionController < ApplicationController
       @name = attributes[:first_name] + " " + attributes[:last_name]
       @email = attributes[:email]
       @affiliation = attributes[:school] + " " + attributes[:college_abbreviation] + " " + attributes[:class_year]
-      @affiliation = "Unknown Affiliation" if @affiliation.blank?
+      # @affiliation = "Unknown Affiliation" if @affiliation.blank?
+      @curriculum = attributes[:major]
+      # @curriculum = "No Curriculum/Major" if @curriculum.blank?
+      @organization = attributes[:organization]
+      # @organization = "No Organization" if @organization.blank?
       @sn_destination_url = "https://yale.service-now.com/incident.do?sys_id=-1&sysparm_query=caller_id=" + sys_id + "^u_contact=" + sys_id
       @phonebook_destination_url = "http://directory.yale.edu/phonebook/index.htm?searchString=upi%3D" + upi
     end
