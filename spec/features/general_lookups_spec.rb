d0888b9b6856ba691170aea259486ca386ca64e9
caseywatts <casey.s.watts@gmail.com>
2014-10-21 14:26:34 -0400

    correct brittle test

--- spec/features/general_lookups_spec.rb
+++ spec/features/general_lookups_spec.rb
@@ -9,7 +9,7 @@ describe 'GeneralLookups', type: :feature do
   it 'can navigate to the general lookups page' do
     visit distribution_personlookup_path
     # brittle but only unique identifier so far
-    expect(page).to have_content 'Card Swipe or NetID or Yale Email'
+    expect(page).to have_content 'Card Swipe or Card Tap or NetID or Yale Email'
   end
 
   it 'can look someone up by netid' do
