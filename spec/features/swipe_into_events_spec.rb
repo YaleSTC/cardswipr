df283d5a19b71e55c5b483abf452cbe47084684f
caseywatts <casey.s.watts@gmail.com>
2014-10-21 12:07:21 -0400

    some integration tests using the testing id numbers

--- spec/features/swipe_into_events_spec.rb
+++ spec/features/swipe_into_events_spec.rb
@@ -14,12 +14,42 @@ describe 'SwipeIntoEvents', type: :feature do
     expect(page).to have_content 'Card Swipe or Card Tap or NetID or Yale Email'
   end
 
-  it 'can swipe someone into the event' do
-    visit event_swipe_path(@event)
-    fill_in 'query', with: 'csw3'
-    click_on('Submit')
-    expect(page).to have_content 'success'
+  context 'using netid' do
+    it 'can swipe someone into the event' do
+      visit event_swipe_path(@event)
+      fill_in 'query', with: 'csw3'
+      click_on('Submit')
+      expect(page).to have_content 'success'
+    end
   end
+
+  context 'using prox card number' do
+    it 'can swipe someone into the event' do
+      visit event_swipe_path(@event)
+      fill_in 'query', with: 'casey.watts@yale.edu'
+      click_on('Submit')
+      expect(page).to have_content 'success'
+    end
+  end
+
+  context 'using prox card number' do
+    it 'can swipe someone into the event' do
+      visit event_swipe_path(@event)
+      fill_in 'query', with: '0099012345'
+      click_on('Submit')
+      expect(page).to have_content 'success'
+    end
+  end
+
+  context 'using magstripe number' do
+    it 'can swipe someone into the event' do
+      visit event_swipe_path(@event)
+      fill_in 'query', with: '9900012345'
+      click_on('Submit')
+      expect(page).to have_content 'success'
+    end
+  end
+
 
   xit 'someone swiped into the event is in the attendance list' do
   end
