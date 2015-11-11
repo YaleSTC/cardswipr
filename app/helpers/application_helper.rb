bc531834da09aee5ec82df82150a2da8cbc5ec86
Seong-June Kim <seong-june.kim@yale.edu>
2015-11-11 11:43:54 -0500

    Refactoring breadcrumbs.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,36 @@ module ApplicationHelper
   def yale_directory_link(upi)
     "http://directory.yale.edu/?queryType=field&upi=#{upi}"
   end
+
+  def breadcrumbs(*labels)
+    links = labels[0...labels.size-1]
+    last = labels.last
+
+    html = '<ol class="breadcrumb">'
+    links.each do |link|
+      html << case link
+      when 'home'
+        "<li>#{link_to 'Home', '/'}</li>"
+      when 'events'
+        "<li>#{link_to 'Events', events_path}</li>"
+      when 'event'
+        "<li>#{link_to @event.title, event_path(@event)}</li>"
+      when 'entry.event'
+        "<li>#{link_to @attendance_entry.event.title, event_attendance_entries_path(@attendance_entry.event)}</li>"
+      else
+        raise "Unrecognized breadcrumb ID #{link}"
+      end
+    end
+
+    html << case last
+    when 'event'
+      "<li>#{@event.title}</li>"
+    when 'entry'
+      "<li>#{@attendance_entry.name}</li>"
+    else
+      "<li>#{last}</li>"
+    end
+
+    html + '</ol>'
+  end
 end
