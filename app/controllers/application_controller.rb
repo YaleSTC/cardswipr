6491ca66552bd9abbfe42dccc5e26db8deb1108b
Matt Coshal <mattcosh@gmail.com>
2019-01-16 15:11:26 -0500

    Add Rubocop.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# Base controller class.
 class ApplicationController < ActionController::Base
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Base mailer class.
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
