8b90419dd5be20d5cc80112baed469a9ef48c349
Shaheer Malik <shaheer.malik@yale.edu>
2019-06-20 16:07:01 -0400

    switching to new env url

--- app/models/fake_api.rb
+++ app/models/fake_api.rb
@@ -2,7 +2,7 @@
 
 class FakeApi
   cattr_accessor :base_url
-  self.base_url = ENV.fetch("FAKE_API_BASE_URL")
+  self.base_url = ENV.fetch("IDENTITY_SERVER_URL")
 
   def successful_match_for(user:)
     HTTParty.get(self.base_url + "users/#{user.id}", format: json)
