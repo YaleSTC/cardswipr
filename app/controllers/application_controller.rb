99be09da8986b91c206309de606b809f902108a0
Niklas Kniesche <niklas.kniesche@yale.edu>
2019-07-16 12:51:47 -0400

    Redirected unauthenticated users to home page
    Resolve #128
    	- note: branchname is different than issue/mr name because this issue changed purpose midcourse

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,15 @@
 
 # Base controller class.
 class ApplicationController < ActionController::Base
-  before_action :redirect_to_sign_in, unless: :public_action?
+  before_action :redirect_to_home_page, unless: :public_action?
 
   private
 
-  def redirect_to_sign_in
+  def redirect_to_home_page
     return if user_signed_in?
 
-    redirect_to new_user_session_path
+    redirect_to root_path
+    flash[:alert] = 'Please sign in to view that page'
   end
 
   def public_action?
--- spec/system/authentication_spec.rb
+++ spec/system/authentication_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Authentication', type: :system do
+  let(:user_with_events) { create(:user_with_events) }
+
   before { stub_cas('NETID') }
 
   describe 'after user is authenticated' do
@@ -19,7 +21,7 @@ RSpec.describe 'Authentication', type: :system do
     end
 
     it 'does not display the sign in link' do
-      expect(page).not_to have_content('Log in')
+      expect(page).not_to have_button('Log in')
     end
   end
 
@@ -27,6 +29,19 @@ RSpec.describe 'Authentication', type: :system do
     it 'does displays the Log in link' do
       visit root_path
       expect(page).to have_button('Log in')
+    end
+
+    it 'redirects to the home page, upon visiting an event page' do
+      id = user_with_events.events[0].id
+      visit event_path(id)
+      check_content
+    end
+  end
+
+  def check_content
+    content = ['Welcome to CardSwipr', 'Please sign in']
+    content.each do |s|
+      expect(page).to have_content(s)
     end
   end
 end
