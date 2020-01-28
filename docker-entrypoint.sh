c2a33f0f06cc292a6b4737f779ce386a6c3a3b18
Matt Coshal <mattcosh@gmail.com>
2020-01-28 14:54:48 -0500

    Remove deco for test deployment to fargate

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,18 +5,6 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
-# variables
-secret_file=/run/secrets/deco.json
- 
-# process deco file
-deco validate $secret_file || exit 1
-deco run $secret_file
-
-# source the new environment variables
-set -a
-. ./.env
-set +a
-
 RAILS_ENV=$RAILS_ENV rails db:create && rails db:migrate  && rails assets:precompile
 
 exec "$@"
