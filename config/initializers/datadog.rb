fe6369286d5515dba1391118854cfc970b731533
Matt Coshal <mattcosh@gmail.com>
2020-03-19 16:59:49 -0400

    Add deco

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -5,5 +5,5 @@ require 'ddtrace'
 Datadog.configure do |c|
   # This will activate auto-instrumentation for Rails
   c.use :rails
-  c.tracer hostname: 'datadog-agent', port: 8126
+  c.tracer hostname: ENV.fetch('DD_AGENT_HOST'), port: 8126
 end
