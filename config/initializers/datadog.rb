269548c7a6d4185bd7d1d4646b1f16fba109e79c
Matt Coshal <mattcosh@gmail.com>
2020-03-20 13:39:08 -0400

    Namespace the application

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -4,6 +4,6 @@ require 'ddtrace'
 
 Datadog.configure do |c|
   # This will activate auto-instrumentation for Rails
-  c.use :rails
+  c.use :rails, service_name: 'cardswipr'
   c.tracer hostname: ENV.fetch('DD_AGENT_HOST'), port: 8126
 end
