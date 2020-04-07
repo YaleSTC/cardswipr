61f4e33e9143c7cf7c26eba1187312af0e6392e1
annacraven <anna.craven@yale.edu>
2020-04-06 22:56:31 -0400

    [221] update attendance creator to handle events with preregistration
    Resolves #221

--- app/services/attendance_creator.rb
+++ app/services/attendance_creator.rb
@@ -2,6 +2,7 @@
 
 # Service object to create an Attendance
 class AttendanceCreator
+  include ActiveModel::Model
   attr_accessor :attendance
   # Initialize an AttendanceCreator
   #
@@ -18,16 +19,31 @@ class AttendanceCreator
       @event.update!(updated_at: Time.zone.now)
       true
     end
-  rescue ActiveRecord::RecordInvalid, RuntimeError
+  rescue StandardError, ActiveRecord::RecordInvalid
+    errors.add(:base, 'Check-in failed')
     false
   end
 
+  private
+
   def create_attendance(search_param)
     person = PeopleHub::PersonRequest.get(search_param)
+    check_preregistrations(person) if @event.preregistration
     @event.attendances.create!(
       first_name: person.first_name, last_name: person.last_name,
       email: person.email, net_id: person.net_id, upi: person.upi,
       phone: person.phone, checked_in_at: Time.zone.now
     )
+  end
+
+  # checks if person has preregistered for event
+  # raises exception if person has not preregistered
+  def check_preregistrations(person)
+    prereg = @event.preregistrations.find_by(net_id: person.net_id)
+    unless prereg
+      errors.add(:base, 'This person has not preregistered')
+      raise StandardError, 'No preregistration found'
+    end
+    prereg.update(checked_in: true)
   end
 end
--- spec/services/attendance_creator_spec.rb
+++ spec/services/attendance_creator_spec.rb
@@ -33,4 +33,43 @@ RSpec.describe AttendanceCreator do
     stub_people_hub_with(net_id)
     expect { creator.call }.to change(event, :updated_at)
   end
+
+  describe 'for event with pregistration' do
+    let(:prereg_event) { create(:event, preregistration: true) }
+    let(:email) { 'email222@email.com' }
+
+    it 'can create attendance' do
+      create(:preregistration, event: prereg_event, net_id: net_id)
+      creator2 = described_class.new(event: prereg_event, search_param: net_id)
+      stub_people_hub_with(net_id)
+      creator2.call
+      expect(creator2.attendance.first_name).to eq('Luke')
+    end
+
+    it 'sets checked_in to true on corresponding preregistration' do
+      prereg = create(:preregistration, event: prereg_event, net_id: net_id)
+      creator2 = described_class.new(event: prereg_event, search_param: net_id)
+      stub_people_hub_with(net_id)
+      creator2.call
+      expect(prereg.reload.checked_in).to eq(true)
+    end
+
+    it 'fails to create attendance for non-preregistered person' do
+      proxno = '0000000001'
+      creator2 = described_class.new(event: prereg_event, search_param: proxno)
+      stub_people_hub_with(proxno)
+      expect(creator2.call).to eq(false)
+    end
+
+    # rubocop:disable RSpec/ExampleLength
+    it 'fails to create attendance for person who has already checked in' do
+      create(:preregistration, event: prereg_event, net_id: net_id,
+                               email: email, checked_in: true)
+      create(:attendance, event: prereg_event, email: email)
+      creator2 = described_class.new(event: prereg_event, search_param: net_id)
+      stub_people_hub(email)
+      expect(creator2.call).to eq(false)
+    end
+    # rubocop:enable RSpec/ExampleLength
+  end
 end
