ef22119eaaf3cd166e174578ec4e58d5907f9e83
mpapallo <michaelapapallo@gmail.com>
2020-04-22 21:02:06 -0400

    Resolves #224

--- app/controllers/preregistrations_controller.rb
+++ app/controllers/preregistrations_controller.rb
@@ -2,10 +2,21 @@
 
 # controller for Preregistration
 class PreregistrationsController < ApplicationController
-  prepend_before_action :set_event, only: %i(index)
+  prepend_before_action :set_event, only: %i(index destroy)
 
   def index
     @preregistrations = @event.preregistrations.order(last_name: :asc)
+  end
+
+  def destroy
+    preregistration = Preregistration.find(params[:id])
+    if preregistration.destroy
+      redirect_to event_preregistrations_path(@event),
+                  notice: 'Successfully deleted preregistration!'
+    else
+      redirect_to event_preregistrations_path(@event),
+                  alert: 'Failed to delete preregistration'
+    end
   end
 
   private
--- app/policies/preregistration_policy.rb
+++ app/policies/preregistration_policy.rb
@@ -5,4 +5,8 @@ class PreregistrationPolicy < ApplicationPolicy
   def index?
     user_can_modify_event?(user, record.event)
   end
+
+  def destroy?
+    index?
+  end
 end
